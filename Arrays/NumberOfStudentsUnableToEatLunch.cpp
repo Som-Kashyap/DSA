649d49430f3686097b927e46742aa289f963f73b
Som-Kashyap <somkashyap0123@gmail.com>
2026-04-23 07:33:45 +0530

    updated : number of students unable to eat lunch

--- Arrays/NumberOfStudentsUnableToEatLunch.cpp
+++ Arrays/NumberOfStudentsUnableToEatLunch.cpp
@@ -1,3 +1,11 @@
+
+// Problem : Number of students unable to eat lunch
+
+// Approach : Initialized an array count with size 2 and elements 0 ( as we have two choices 0 and 1 ) , count of each choice is stored by iterating through the given students vector. Ans variable is initiated with size of sandwiches vector.
+
+//  If front element of the vectors match , count is decreased and also the ans variable. If either vector becomes null , we break out of the loop as no possibilities are left.
+ 
+
 #include<iostream>
 #include<vector>
 
