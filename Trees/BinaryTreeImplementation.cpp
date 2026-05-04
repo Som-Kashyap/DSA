cb83cc3af77e894417a5c8b1854c3994a3ddd2cf
Som-Kashyap <somkashyap0123@gmail.com>
2026-05-04 21:19:07 +0530

    added pre-order traversal

--- Trees/BinaryTreeImplementation.cpp
+++ Trees/BinaryTreeImplementation.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<vector>
 
 using namespace std;
 
@@ -20,13 +21,35 @@ struct Node {
 
 };
 
+void preorder ( Node* root , vector<int> &res ) {
+
+    if ( root == NULL ) return;
+
+    cout<<root->data<<endl;
+
+    preorder ( root->left , res );
+
+    preorder ( root->right , res );
+
+}
+
 int main () {
+
+    vector<int>res;
 
     struct Node* root = new Node(1);
     root->left = new Node(2);
     root->right = new Node(3);
 
     root->left->right = new Node(4);
+
+    preorder ( root , res );
+
+    for ( int node: res ) {
+
+        cout<<node<<" ";
+
+    }
 
     return 0;
 
