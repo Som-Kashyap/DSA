5655c8f133d86a92dd260d62b109f66c59c65a03
Som-Kashyap <somkashyap0123@gmail.com>
2026-05-06 07:47:23 +0530

    updated max depth file

--- Trees/MaxDepthOfBinaryTree.cpp
+++ Trees/MaxDepthOfBinaryTree.cpp
@@ -40,7 +40,6 @@ int main () {
 
     struct Node* root = new Node(1);
     root->left = new Node(4);
-    root->left = new Node(2);
     root->left->right = new Node(5);
     root->left->right->left = new Node(3);
     root->right->left = new Node(7);
