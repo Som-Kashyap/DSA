cbf7df1a1d1268d2d796cf377b1b038b5114651e
Som-Kashyap <somkashyap0123@gmail.com>
2026-05-06 09:27:00 +0530

    added: in-order traversal iterative

--- Trees/InOrderTraversal-Iterative.cpp
+++ Trees/InOrderTraversal-Iterative.cpp
@@ -15,6 +15,60 @@ struct Node {
         data = val;
         left = NULL;
         right = NULL;
-        
+
     }
 };
+
+vector<int> inOrder ( Node* root ) {
+
+    vector<int>res;
+    stack<Node*>st;
+
+    Node* node = root;
+
+    while ( !st.empty() || node ) {
+
+        while ( node ) {
+
+        st.push( node);
+
+        node = node->left;
+
+        }
+
+        if ( !st.empty() ) {
+
+    node = st.top();
+    st.pop();
+
+    res.push_back( node->data );
+
+    node = node->right;
+    
+    }
+}
+
+    return res;
+}
+
+int main () {
+
+    struct Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->right->left = new Node(6);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+
+   vector<int>result = inOrder ( root );
+
+   for ( int val : result ) {
+
+    cout<<val<<" ";
+
+   }
+   cout<<endl;
+
+    return 0;
+    
+}
