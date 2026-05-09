f2e763c7d8a9c21ea326a2913890f337224710ac
Som-Kashyap <somkashyap0123@gmail.com>
2026-05-09 11:00:11 +0530

    added : max path sum

---
+++ Trees/MaximumPathSum.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<climits>
+
+using namespace std;
+
+struct Node{
+
+    int data;
+    Node* left ;
+    Node* right ;
+
+    Node( int val ) {
+
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+int MaxPathSum( Node* root , int &maxsum ) {
+
+    if ( root == NULL ) return 0;
+
+    int ls = max( 0 , MaxPathSum ( root->left , maxsum )) ; 
+    int rs = max ( 0 , MaxPathSum( root->right, maxsum  ));
+
+    maxsum = max( maxsum , (root->data + ls + rs) );
+
+    return root->data + max ( ls , rs );
+
+}
+
+int main () {
+
+    struct Node* root = new Node(-10);
+    root->left = new Node(9);
+    root->right = new Node(20);
+    root->right->left = new Node(15);
+    root->right->right = new Node(7);
+
+    int maxsum = INT_MIN;
+
+   MaxPathSum(root, maxsum);
+
+   cout << "Max Path Sum: " << maxsum << endl;
+
+    return 0;
+
+}
