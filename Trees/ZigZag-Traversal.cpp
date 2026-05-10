04043b0be058f516e2808d814952635867d4ec3c
Som-Kashyap <somkashyap0123@gmail.com>
2026-05-10 08:19:58 +0530

    added: zig-zag order traversal

---
+++ Trees/ZigZag-Traversal.cpp
@@ -0,0 +1,90 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+
+using namespace std;
+
+struct TreeNode{
+
+    int val;
+     TreeNode* left;
+     TreeNode* right;
+
+    TreeNode( int data ) {
+
+        val=data;
+        left = NULL;
+        right = NULL;
+
+    }
+
+};
+
+vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        
+        
+        queue<TreeNode*>q;
+        vector<vector<int>>ans;
+
+          if ( !root ) return ans;
+
+        bool LeftToRight = true;
+
+        q.push( root );
+
+        while ( !q.empty() ) {
+
+            int size = q.size();
+            vector<int>level(size);
+
+            for ( int i = 0; i < size; i++ ) {
+
+                TreeNode* node = q.front();
+
+                q.pop();
+
+                int index = (LeftToRight ? i : size-i-1);
+
+                level[index] = node->val;
+
+                 if ( node->left ) q.push ( node->left );
+
+                if ( node->right ) q.push ( node->right );
+
+            }
+
+            LeftToRight = !LeftToRight;
+             ans.push_back ( level );
+
+        }
+        return ans;
+    }
+
+void print ( const vector<vector<int>>& ans ) {
+
+        for ( auto &level : ans ) {
+
+            for ( int val : level ) {
+
+                cout<<val<<" ";
+
+            }
+            cout<<endl;
+        }
+    }
+
+   int main () {
+
+        struct TreeNode* root = new TreeNode(3);
+        root->left = new TreeNode(9);
+        root->right = new TreeNode(20);
+        root->right->left = new TreeNode(15);
+        root->right->right = new TreeNode(7);
+
+        vector<vector<int>> ans = zigzagLevelOrder ( root );
+     
+        print ( ans );
+
+        return 0;
+
+    }
