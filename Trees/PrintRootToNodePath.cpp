52ecba04e2c86e01e8517175c7ab5a969c8f079d
Som-Kashyap <somkashyap0123@gmail.com>
2026-05-12 10:37:02 +0530

    added: print root to node path

---
+++ Trees/PrintRootToNodePath.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+struct Node {
+
+    int val;
+    Node* left;
+    Node* right;
+
+    Node ( int data ) {
+
+        val = data;
+        left = NULL ; right = NULL;
+
+    }
+
+};
+
+bool RootToNodePath ( Node* root , vector<int> &path , int &target  ) {
+
+    if ( root == NULL ) {
+
+        return false;
+
+    }
+
+    path.push_back ( root->val );
+
+    if ( root->val == target ) return true;
+
+    if ( RootToNodePath ( root->left , path , target ) || RootToNodePath ( root->right , path , target ) ) return true;
+
+    path.pop_back ();
+    
+    return false;
+
+}
+
+int main () {
+
+    vector<int>path;
+    int target = 7;
+
+    struct Node* root = new Node(1);
+    root->left = new Node(2);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->left->right->left = new Node(6);
+    root->left->right->right = new Node(7);
+
+    if ( !root ) {
+
+        throw runtime_error ("Root is NULL!");
+
+    }
+
+    RootToNodePath ( root , path , target );
+
+    for ( int pathnodes : path ) {
+
+        cout<<pathnodes<<" ";
+
+    }
+    cout<<endl;
+
+    return 0;
+
+}
