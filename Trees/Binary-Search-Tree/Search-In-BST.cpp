90a175c6872b7609005a74440f5a270dd5f3e573
Som-Kashyap <somkashyap0123@gmail.com>
2026-05-21 07:37:32 +0530

    added: search in BST

---
+++ Trees/Binary-Search-Tree/Search-In-BST.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+
+using namespace std;
+
+
+struct Node {
+
+    int val;
+    Node* left;
+    Node* right;
+
+    Node( int data ) {
+
+        val = data;
+        left = NULL;
+        right = NULL;
+
+    }
+};
+
+Node* searchBST ( Node* root , int target ) {
+
+    if ( !root ) return NULL;
+
+    if ( root->val == target ) return root;
+
+    if ( root->val > target )  return searchBST( root->left , target );
+
+    else return searchBST( root->right , target );
+
+}
+
+int main () {
+
+    struct Node* root = new Node(1);
+    root->right = new Node(3);
+    root->left = new Node(2);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->left->right->right = new Node(6);
+
+    int target = 5;
+
+    searchBST ( root , target );
+
+    return 0;
+
+}
