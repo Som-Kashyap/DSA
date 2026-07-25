780d680ffb197e3491404f2cc14d0840779c097d
Som-Kashyap <somkashyap0123@gmail.com>
2026-07-25 06:20:50 +0530

    update: add union by size

--- Graph/Disjoint-Set.cpp
+++ Graph/Disjoint-Set.cpp
@@ -5,15 +5,20 @@ using namespace std;
 class DisjointSet{
 
     vector<int> parent; vector<int> rank;
+    vector<int> size;
 
     public:
 
     DisjointSet(int n) {
 
-        parent.resize(n,0);
-        rank.resize(n,0);
+        parent.resize(n+1,0);
+        rank.resize(n+1,0);
+        size.resize(n+1,0);
 
-        for ( int i = 0; i <= n; i++ ) parent[i] = i;
+        for ( int i = 0; i <= n; i++ ) {
+            parent[i] = i;
+            size[i] = i;
+        }
 
     }
 
@@ -26,21 +31,40 @@ class DisjointSet{
         return parent[node] = FindUltimateParent(parent[node]); //Path Compression
     }
 
-    void UnionByRank ( int u, int v ) {
+     void UnionByRank ( int u, int v ) { //TC: O(4*alpha)
 
         int ult_u = FindUltimateParent (u);
         int ult_v = FindUltimateParent (v);
 
        if ( ult_u == ult_v ) return;
 
-       if ( rank[u] < rank[v] ) parent[u] = v;
+       if ( rank[u] < rank[v] ) parent[ult_u] = ult_v;
 
-       else if ( rank[v] > rank[u] ) parent[v] = u;
+       else if ( rank[v] > rank[u] ) parent[ult_v] = ult_u;
 
        else {
-        parent[v] = u;
+        parent[ult_v] = ult_u;
         rank[u]++;
        }
+    }
+
+    void UnionBySize ( int u, int v ) { //TC: O(4*alpha)
+
+        int ult_u = FindUltimateParent (u);
+        int ult_v = FindUltimateParent (v);
+
+        if ( size[ult_u] < size[ult_v] ) {
+
+            parent[ult_u] = ult_v;
+            size[ult_v] += size[ult_u];
+
+        }
+
+        else {
+
+            parent[ult_v] = ult_u;
+            size[ult_u] += size[ult_v]; 
+        }
     }
 
 };
